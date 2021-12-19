e10704f44a8c96df10492ea7a9356fb5c8b08f5a
tamton-aquib <aquibjavedt007@gmail.com>
2021-12-19 12:02:51 +0530

    adding projects

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # graveyard
 Some of my small rust side projects
+
+---
+#### line_count
+To line counts obv. <br />
+`status: complete`
+
+---
+#### list
+To list files and folders with colors. <br />
+`status: complete` (but can be extended)
+
+---
+#### wifi_menu
+To list active wifi interfaces in a gui. (uses [egui](https://github.com/emilk/egui) lib). <br />
+`status: incomplete`
+
---
+++ list/src/main.rs
@@ -0,0 +1,31 @@
+use std::{io, fs, env};
+use std::path::Path;
+mod icon;
+
+fn real_main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let dir = if args.len() < 2 { "." } else { &args[1] };
+
+    let entries = fs::read_dir(&dir)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    let mut nice_list: Vec<String> = vec![];
+    for entry in &entries {
+        let filename = Path::new(entry);
+        let str_filename = &filename.file_name().unwrap().to_str().unwrap().to_string();
+        let is_dir = filename.is_dir();
+
+        let icon = icon::get_fileicon(filename, is_dir);
+        let item = format!("{} {}", icon, str_filename);
+
+        nice_list.push(item);
+    }
+    println!("{}", nice_list.join("   "));
+
+    Ok(())
+}
+
+fn main() {
+    real_main().unwrap();
+}
