fb2af3403f6c96e9fd4d7e83fb7697b03514b035
tamton-aquib <aquibjavedt007@gmail.com>
2022-05-01 18:11:16 +0530

    fix(colors): changed clipboard crate for x11 compatibility.

--- colors/src/utils/mod.rs
+++ colors/src/utils/mod.rs
@@ -1,13 +1,18 @@
-use terminal_clipboard;
+// use terminal_clipboard;
+use clipboard_ext::prelude::*;
 
-use chrono::{DateTime, Utc};
+// use chrono::{DateTime, Utc};
 use std::fs;
 use std::process::Command;
 
 // TODO: copy to clipboard
 pub fn copy_to_clip(content: &str) {
-    println!("Copied: {}", terminal_clipboard::get_string().unwrap());
-    terminal_clipboard::set_string(content).expect("Couldnt Copy to Clipboard!");
+    // println!("Copied: {}", terminal_clipboard::get_string().unwrap());
+    // terminal_clipboard::set_string(content).expect("Couldnt Copy to Clipboard!");
+    clipboard_ext::x11_bin::ClipboardContext::new()
+        .unwrap()
+        .set_contents(content.to_string())
+        .unwrap();
 }
 pub fn to_hex(v: &Vec<u8>) -> String {
     format!("#{:02x}{:02x}{:02x}", &v[0], &v[1], &v[2])
@@ -21,9 +26,9 @@ pub fn delete_screenshot(file: String) {
 }
 
 pub fn screenshot() -> String {
-    let now: DateTime<Utc> = Utc::now();
+    // let now: DateTime<Utc> = Utc::now();
     // let filename = now.format("screenshot%d_%m_%Y__%H_%M_%S.png").to_string();
-    let filename = now.format("./screenshot.png").to_string();
+    let filename = String::from("./screenshot.png");
 
     Command::new("scrot")
         .args(&["-z", &filename])
