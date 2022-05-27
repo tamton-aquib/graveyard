f3e6b4a6985b3e0b2b7bb1c3a0d6d3a7454c8d25
tamton-aquib <aquibjavedt007@gmail.com>
2022-05-27 17:03:27 +0530

    nearing completion for `colors`

---
+++ colors/src/utils.rs
@@ -0,0 +1,15 @@
+use clipboard_ext::prelude::*;
+
+pub fn copy_to_clip(content: &str) {
+    clipboard_ext::x11_bin::ClipboardContext::new()
+        .unwrap()
+        .set_contents(content.to_string())
+        .unwrap();
+}
+
+pub fn to_hex(v: &Vec<u8>) -> String {
+    format!("#{:02x}{:02x}{:02x}", &v[0], &v[1], &v[2])
+}
+pub fn to_rgb(v: &Vec<u8>) -> String {
+    format!("rgb({},{},{})", &v[0], &v[1], &v[2])
+}
