0105aa55df1b21e4d73b3f5da227860b0de33657
tamton-aquib <aquibjavedt007@gmail.com>
2022-07-11 22:24:57 +0530

    adding icons to list/ and updated readme for ctf_utils

--- list/src/icon.rs
+++ list/src/icon.rs
@@ -11,6 +11,7 @@ pub fn get_fileicon(filename: &Path, is_dir: bool) -> String {
             "lua" => "".blue().to_string(),
             "py" => "".blue().to_string(),
             "cpp" => "".bright_blue().to_string(),
+            "c" => "".bright_blue().to_string(),
             "toml" => "".white().to_string(),
             "conf" => "".red().to_string(),
             "rs" => "".truecolor(211, 158, 129).to_string(),
@@ -20,9 +21,12 @@ pub fn get_fileicon(filename: &Path, is_dir: bool) -> String {
             "java" => "".truecolor(218, 106, 3).to_string(),
             "json" => "".truecolor(245, 200, 63).to_string(),
             "js" => "".truecolor(232, 213, 82).to_string(),
+            "norg" => "".truecolor(72, 120, 190).to_string(),
 
             _ => "".to_string(),
         },
         None => "".to_string(),
     }
 }
+// yml,lock,dart,md,ts
+// git,makefile,bash,svelte,react,sql
